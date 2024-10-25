3ed8efd9c5e96faf7be148c5cc517a8f97c46be2
Ibrahim Khalil <ibrahimkholil01@gmail.com>
2024-10-25 11:41:43 +0600

    JSon

--- MVC-MIS/Controllers/HomeController.cs
+++ MVC-MIS/Controllers/HomeController.cs
@@ -12,37 +12,43 @@ namespace MVC_MIS.Controllers
         {
             return View();
         }
+        public JsonResult GenName()
+        {
+            return Json("Ibrahim Khalil", JsonRequestBehavior.AllowGet);
+        }
 
         public ActionResult About()
         {
             ViewBag.Message = "My Fresh Load";
-
+       
             return View();
         }
 
         [HttpPost]
         public ActionResult About(string btnSubmit, FormCollection formCollection)
         {
-            int hdTotalRow = Convert.ToInt32(formCollection["hdTotalRow"].ToString());
-            for (int i = 0; i < hdTotalRow; i++)
-            {
-                if (formCollection["chk_" + i.ToString()] != null)
-                {
-                    if (formCollection["chk_" + i.ToString()].ToString().ToLower() == "on")
-                    {
-                        string txtName = formCollection["txtName_" + i.ToString()].ToString();
-                        string txtAge = formCollection["txtAge_" + i.ToString()].ToString();
-                        string txtEducation = formCollection["txtEducation_" + i.ToString()].ToString();
-                        string txtHobby = formCollection["txtHobby_" + i.ToString()].ToString();
-                        string ddlSelect = formCollection["ddlSelect_" + i.ToString()].ToString();
-                    }
+            //int hdTotalRow = Convert.ToInt32(formCollection["hdTotalRow"].ToString());
+            //for (int i = 0; i < hdTotalRow; i++)
+            //{
+            //    if (formCollection["chk_" + i.ToString()] != null)
+            //    {
+            //        if (formCollection["chk_" + i.ToString()].ToString().ToLower() == "on")
+            //        {
+            //            string txtName = formCollection["txtName_" + i.ToString()].ToString();
+            //            string txtAge = formCollection["txtAge_" + i.ToString()].ToString();
+            //            string txtEducation = formCollection["txtEducation_" + i.ToString()].ToString();
+            //            string txtHobby = formCollection["txtHobby_" + i.ToString()].ToString();
+            //            string ddlSelect = formCollection["ddlSelect_" + i.ToString()].ToString();
+            //        }
 
-                }
+            //    }
 
-            }
+            //}
 
-            ViewBag.Message = "This is Postback Load.";
-            return View();
+            //this is fast way
+            //ViewBag.formCollection = formCollection;
+            //this is another way.
+            return View(formCollection);
         }
 
         public ActionResult Contact()
